4f9e8df0e4d7ed4d00cf35b343a2e7495b02886c
TangB5 <ndohyannick78@gmail.com>
2024-12-28 22:35:46 +0100

    premier commit front-end

---
+++ dist/module.js
@@ -0,0 +1,35 @@
+const menuToggle = document.getElementById('menu-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+const menuClose = document.getElementById('menu-close'); 
+
+menuToggle.addEventListener('click', () => {
+      mobileMenu.classList.add('top-[12%]');
+      menuToggle.classList.add('hidden');
+      menuClose.classList.remove('hidden');
+    });
+    menuClose.addEventListener('click', () => {
+        mobileMenu.classList.remove('top-[12%]');
+        menuClose.classList.add('hidden');
+        menuToggle.classList.remove('hidden');
+      });
+
+    // Scroll vers les sections
+    document.querySelectorAll('.nav-link').forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            target.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+
+    // Afficher/Masquer le formulaire
+    document.querySelectorAll('.buy-button').forEach(button => {
+        button.addEventListener('click', () => {
+            document.getElementById('form-container').classList.remove('hidden');
+        });
+    });
+
+    document.getElementById('close-form').addEventListener('click', () => {
+        document.getElementById('form-container').classList.add('hidden');
+    });
+
